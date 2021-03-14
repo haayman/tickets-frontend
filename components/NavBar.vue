79671d858bc9cd5e0b68d11befd8804ae7bb8ff3
Arjen Haayman <arjen.haayman@gmail.com>
2021-03-14 12:03:40 +0100

    user.name toegevoegd

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-app-bar app flat>
+    <v-app-bar app>
       <v-container class="py-0 fill-height">
         <v-toolbar-title>
           <nuxt-link :to="{ name: 'index' }" class="v-toolbar__brand">
@@ -16,7 +16,7 @@
 
         <v-spacer />
 
-        <v-btn v-if="isAuthenticated" @click="logout" text>Log uit</v-btn>
+        <v-btn v-if="isAuthenticated" @click="logout" text>Log uit {{ userName }}</v-btn>
         <v-btn v-else :to="{ name: 'login' }" text>Leden login</v-btn>
       </v-container>
     </v-app-bar>
@@ -28,9 +28,13 @@ import { mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(["isAdmin", "isAuthenticated", "isKassa", "isSpeler"]),
+    ...mapGetters(["isAdmin", "isAuthenticated", "isKassa", "isSpeler", "loggedInUser"]),
     title() {
       return "Theater tickets";
+    },
+
+    userName() {
+      return this.loggedInUser?.name || "";
     },
     links() {
       const links = [
