ddf37e31a523de40598b5c5540cbf36446a0b095
Arjen Haayman <arjen.haayman@gmail.com>
2024-11-23 11:13:44 +0100

    'Gesloten' als er geen voorstellingen zijn

--- pages/voorstelling/index.vue
+++ pages/voorstelling/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <v-card>
-    <VoorstellingList v-if="voorstellingen" :voorstellingen="voorstellingen" />
+  <v-card v-if="voorstellingen.length > 1">
+    <VoorstellingList :voorstellingen="voorstellingen" />
+  </v-card>
+  <v-card v-else class="pa-3">
+    <h2>Gesloten</h2>
+    <p>Het is op dit moment niet mogelijk kaarten te kopen voor een voorstelling</p>
   </v-card>
 </template>
 <script setup lang="ts">
